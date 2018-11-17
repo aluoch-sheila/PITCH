99ff8eb47313a3993eab2f0f3f6a2f237320a3fb
aluoch-sheila <aluochsheila1999@gmail.com>
2018-11-17 10:30:28 +0300

    added a profile picture path

--- app/models.py
+++ app/models.py
@@ -13,4 +13,6 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(255),index = True)
     email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    password_hash = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
